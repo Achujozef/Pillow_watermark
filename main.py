b581f0939d3c8c9e05abc333b7823505d56e4ff0
achujoseph0709 <achujozefsl0709@gmail.com>
2023-12-03 23:42:59 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from PIL import Image,ImageDraw, ImageFont
+
+
+def add_watermark():
+    pass
+
+
+
+
+input_image = 'input.jpeg'
+output_image ='output.png'
+text = input("Enter Your Watermakr Text")
+
+
+add_watermark(input_image,output_image,text)
