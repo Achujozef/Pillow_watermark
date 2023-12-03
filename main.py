6673c1b7842f7adf0c5f24eb4c8a2c945baa5f5e
achujoseph0709 <achujozefsl0709@gmail.com>
2023-12-03 23:52:36 +0530

    Done

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from PIL import Image,ImageDraw, ImageFont
 
 
-def add_watermark():
-    pass
+def add_watermark(input_image,output_image,text):
+    orginal_image = Image.open(input_image)
+    water_mark_img = Image.new("RGBA",orginal_image.size)
+    watermark_draw =ImageDraw.Draw(water_mark_img)
+    font =ImageFont.truetype('arial.ttf',100)
+    width,height =orginal_image.size
+    watermark_draw.text((0,height/2),text=text,fill=(255,255,255,140),font=font)
 
-
+    water_marked_image = Image.alpha_composite(im1=orginal_image.convert("RGBA"),im2=water_mark_img)
+    water_marked_image.save(output_image,"PNG")
+    print("Watermarked Image Genarated")
 
 
 input_image = 'input.jpeg'
